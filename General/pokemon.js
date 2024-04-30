f1264c333391ca948564628295978389f8e23990
mario-rio <mario.munteanu@cirvianum.cat>
2024-04-30 16:11:28 +0200

    prova2

--- General/pokemon.js
+++ General/pokemon.js
@@ -1,2 +1,19 @@
 // Afegeix aquí el codi de JS per a la pàgina pokemon.html
-// provamario
+
+
+async function getPokemonInfo(idOrName) {
+    try {
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${idOrName}`);
+      if (!response.ok) {
+        throw new Error(`Error: ${response.status}`);
+      }
+      const data = await response.json();
+      return {
+        id: data.id,
+        
+
+      };
+    } catch (error) {
+
+    }
+  }
